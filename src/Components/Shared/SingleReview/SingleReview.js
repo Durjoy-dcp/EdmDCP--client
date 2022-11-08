e5a15b52109c03f1b3688c29abf1bf536bba517e
Durjoy-dcp <unknownccxv@gmail.com>
2022-11-08 22:45:51 +0600

    textarea and break of word wrap

--- src/Components/Shared/SingleReview/SingleReview.js
+++ src/Components/Shared/SingleReview/SingleReview.js
@@ -5,14 +5,17 @@ const SingleReview = ({ singlerev }) => {
     const { review, serviceId, img, name, date } = singlerev;
 
     return (
-        <div>
+        <div className=''>
             <div className='d-flex align-items-center my-2 '>
 
                 <img src={img} className='rounded-circle' alt="" style={{ width: "40px", height: "40px" }} />
                 <p className='fs-5 my-2 px-2'>{name}</p>
             </div>
-            <p className='fw-bolder'>{review} </p>
-            <p> <Moment fromNow>{date}</Moment></p>
+            <p className='fw-bolder' style={{ overflowWrap: "break-word" }}>{review} </p>
+            <div className='d-flex justify-content-end'>
+
+                <p className='p-1'> <Moment fromNow>{date}</Moment></p>
+            </div>
             <hr />
         </div>
     );
